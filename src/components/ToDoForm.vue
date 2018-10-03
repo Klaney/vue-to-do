a53bf421093f850802e6323805c035999d461396
Klaney <Keatonus@gmail.com>
2018-10-02 22:34:19 -0600

    got a component form with two way data binding working

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="to-do-form">
+      <input v-model="title">
+      <input v-model="description">
+      <!-- <input type="submit" value="create"/> -->
+    <p>
+      title is now {{title}}, description is {{description}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+  data: function() {
+    return {
+      title: "",
+      description: ""
+    };
+  }
+};
+</script>
